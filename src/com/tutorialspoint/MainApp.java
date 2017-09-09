206cec69ed4eb5693c78cf6ca591fc7b9c670c89
lisssb <liss.sb@gmail.com>
2017-09-09 12:22:40 +0200

    bean inheritance

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -1,19 +1,19 @@
 package com.tutorialspoint;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
    public static void main(String[] args) {
-	   AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
       
-      
-      HelloWorld obj2 = (HelloWorld) context.getBean("helloWorld1");
-   
-      obj.getMessage();
-      obj2.getMessage();
-      context.registerShutdownHook(); //This will ensure a graceful shutdown and call the relevant destroy methods.
+      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+      objA.getMessage1();
+      objA.getMessage2();
+
+      HelloIndia objB = (HelloIndia) context.getBean("helloIndia");
+      objB.getMessage1();
+      objB.getMessage2();
+      objB.getMessage3();
    }
 }
