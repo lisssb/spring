b1bb5174bf7bde0ddeed38c614267f13c1718c47
lisssb <liss.sb@gmail.com>
2017-09-22 20:39:59 +0200

    Declarative transaction management

--- src/com/tutorialspoint/transaction/MainApp.java
+++ src/com/tutorialspoint/transaction/MainApp.java
@@ -8,10 +8,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("BeansTransaction.xml");
-		StudentJDBCTemplate studentJDBCTemplate = (StudentJDBCTemplate) context.getBean("studentJDBCTemplate");
+		// here I have to use interface instead class
+		StudentDAO studentJDBCTemplate = (StudentDAO) context.getBean("studentJDBCTemplate");
 
 		System.out.println("------Records creation--------");
-		studentJDBCTemplate.create("Zara", 11, 99, 2010);
+		studentJDBCTemplate.create("Lissete", 11, 99, 2010);
 		studentJDBCTemplate.create("Nuha", 20, 97, 2010);
 		studentJDBCTemplate.create("Ayan", 25, 100, 2011);
 
--- src/com/tutorialspoint/transaction/StudentJDBCTemplate.java
+++ src/com/tutorialspoint/transaction/StudentJDBCTemplate.java
@@ -6,29 +6,15 @@ import javax.sql.DataSource;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class StudentJDBCTemplate implements StudentDAO {
-	private DataSource dataSource;
 	private JdbcTemplate jdbcTemplateObject;
-	private PlatformTransactionManager transactionManager;
 
 	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
 		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
 	}
 
-	public void setTransactionManager(PlatformTransactionManager transactionManager) {
-		this.transactionManager = transactionManager;
-	}
-
 	public void create(String name, Integer age, Integer marks, Integer year) {
-		TransactionDefinition def = new DefaultTransactionDefinition();
-		TransactionStatus status = transactionManager.getTransaction(def);
-
 		try {
 			String SQL1 = "insert into Student (name, age) values (?, ?)";
 			jdbcTemplateObject.update(SQL1, name, age);
@@ -41,13 +27,12 @@ public class StudentJDBCTemplate implements StudentDAO {
 			jdbcTemplateObject.update(SQL3, sid, marks, year);
 
 			System.out.println("Created Name = " + name + ", Age = " + age);
-			transactionManager.commit(status);
+			// to simulate the exception.
+			// throw new RuntimeException("simulate Error condition");
 		} catch (DataAccessException e) {
 			System.out.println("Error in creating record, rolling back");
-			transactionManager.rollback(status);
 			throw e;
 		}
-		return;
 	}
 
 	public List<StudentMarks> listStudents() {
